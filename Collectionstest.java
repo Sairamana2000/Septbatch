b8089d16f18f77b80cbd1244099c2a84aafee2d8
sairamana <venkatasai97036@gmail.com>
2023-11-08 16:31:06 +0530

    data

---
+++ Collectionstest.java
@@ -0,0 +1,27 @@
+https://github.com/Sairamana2000/Septbatch.git
+  
+
+import java.util.*;
+   public class Collectionstest.java
+     {
+
+    public static void main(String[] args)
+    {
+
+
+    }
+
+
+     }
+000/Septbatch.git
+~
+000/Septbatch.git
+000/Septbatch.git
+000/Septbatch.git
+000/Septbatch.git
+000/Septbatch.git
+~
+~
+~
+~
+~
